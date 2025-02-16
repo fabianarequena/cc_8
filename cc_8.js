18211dffb0ddd1ff4f04fdfbd17646954be5dece
fabianarequena <fabianarequena@usf.edu>
2025-02-16 13:11:09 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -1,9 +1,9 @@
-//Task 1: Employee Salary Calculation
+// Task 1: Employee Salary Calculation
 
 function calculateSalary(baseSalary, bonus, taxRate) {
     let netSalary = (baseSalary + bonus) - (baseSalary * taxRate);
     return `Net Salary: $${netSalary.toFixed(2)}`;
-}
+};
 
 console.log(calculateSalary(5000, 500, 0.1)); // Expected output: "Net Salary: $5000.00"
 console.log(calculateSalary(7000, 1000, 0.15)); // Expected output: "Net Salary: $6950.00"
@@ -11,3 +11,14 @@ console.log(calculateSalary(7000, 1000, 0.15)); // Expected output: "Net Salary:
 // Created a function that calculates the net salary by adding the base salary and bonus, then subtracting the tax amount
 
 
+// Task 2: Product Price After Discount
+
+function calculateDiscount(price, discountRate) {
+    let finalPrice = price - (price * discountRate);
+    return `Final Price: $${finalPrice.toFixed(2)}`;
+};
+
+console.log(calculateDiscount(100, 0.2)); // Expected output: "Final Price: $80.00"
+console.log(calculateDiscount(250, 0.15)); // Expected output: "Final Price: $212.50"
+
+// This function calculates the final price of a product after applying a discount
