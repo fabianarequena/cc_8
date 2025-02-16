ad666c44b1618927dcc6632b43c3922510c9c39b
fabianarequena <fabianarequena@usf.edu>
2025-02-16 13:07:59 -0500

    Task 1 - Employee Salary Calculation ***RE-COMMIT DISREGARD PREVIOUS***

--- cc_8.js
+++ cc_8.js
@@ -1,7 +1,13 @@
-//Task 1: Function Declaration - Employee Salary Calculation
+//Task 1: Employee Salary Calculation
+
 function calculateSalary(baseSalary, bonus, taxRate) {
     let netSalary = (baseSalary + bonus) - (baseSalary * taxRate);
     return `Net Salary: $${netSalary.toFixed(2)}`;
 }
 
-//Created function calculates the net salary by adding the base salary and bonus, then subtracting the tax amount.
+console.log(calculateSalary(5000, 500, 0.1)); // Expected output: "Net Salary: $5000.00"
+console.log(calculateSalary(7000, 1000, 0.15)); // Expected output: "Net Salary: $6950.00"
+
+// Created a function that calculates the net salary by adding the base salary and bonus, then subtracting the tax amount
+
+
